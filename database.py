bc68d21972435b9bf8811343ef012848014ee4e5
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2022-04-16 21:51:39 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-import psycopg as ps
+import psycopg2 as ps
 import requests
 
 time_series_daily = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=compact&apikey=YW9W7ZBX5RBNC6V7'
@@ -16,3 +16,7 @@ class database:
   
   def connect(self):
     connection = ps.connect(host=self.host,user=self.user,password=self.password,database=self.database,port=self.port)
+    return connection
+  
+db = database('ec2-23-20-224-166.compute-1.amazonaws.com', 'zsuqqgfjipwpwu', '98b25a8a11bf2eb034266f33871bcc966d3a4217d37ed4e3f542bdccf1717c79', 'db1apeq0n8lsoa', '5432')
+print(db.connect())
