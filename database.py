2495110f2613646fb18c6916d3215135a458bf93
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2022-04-16 21:15:11 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,11 @@
 import psycopg as ps
 import requests
+
+class database:
+  # When calling the class, the variables in the __init__ function are also inputted by the user
+  def __init__(self, host, user, password, database, port):
+    self.host = host
+    self.user = user
+    self.password = password
+    self.database = database
+    self.port = port
