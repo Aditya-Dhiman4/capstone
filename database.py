7912c735ad9b25d4fc2bec893a12919472fb23fe
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2022-04-21 00:21:25 -0700

    Update database.py

--- database.py
+++ database.py
@@ -18,19 +18,35 @@ class database:
   def stock_data(self, symbol):
     # retriving json file with price and volume data
     time_series_daily = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=compact&apikey=YW9W7ZBX5RBNC6V7'
-    r = requests.get(time_series_daily)
-    data = r.json()
-    
+    overview = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey=YW9W7ZBX5RBNC6V7'
+    r_tsd = requests.get(time_series_daily)
+    r_o = requests.get(overview)
+    time_series_data = r_tsd.json()
+    overview_data = r_o.json()
+
+    keys = list(overview_data.keys())
+    del keys[1:13]
+    del keys[27:-1]
+    del keys[-1]
+    new_keys = []
+
+    for key in keys:
+        new_keys.append(key.lower())
+
+    new_keys.append('yearly_weekhigh')
+    new_keys.append('yearly_weeklow')
+
+    print(new_keys)
     # returns last 30 trading days
-    trade_dates = list(data['Time Series (Daily)'].keys())[0:30]
+    trade_dates = list(time_series_data['Time Series (Daily)'].keys())[0:30]
     
     # iterating through the last 30 trading dates to put price and volume data into database
     for dates in trade_dates:
-        open = data['Time Series (Daily)'][dates]['1. open']
-        high = data['Time Series (Daily)'][dates]['2. high']
-        low = data['Time Series (Daily)'][dates]['3. low']
-        close = data['Time Series (Daily)'][dates]['4. close']
-        volume = data['Time Series (Daily)'][dates]['5. volume']
+        open = time_series_data['Time Series (Daily)'][dates]['1. open']
+        high = time_series_data['Time Series (Daily)'][dates]['2. high']
+        low = time_series_data['Time Series (Daily)'][dates]['3. low']
+        close = time_series_data['Time Series (Daily)'][dates]['4. close']
+        volume = time_series_data['Time Series (Daily)'][dates]['5. volume']
         
         command = f'''
             insert into public.all_stock_data (
