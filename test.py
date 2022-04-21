cd6027afeb20b1eaec6f6092bfd47ab410e71443
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2022-04-20 23:03:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,15 @@ r2 = requests.get(overview)
 overview_data = r2.json()
 
 keys = list(overview_data.keys())
-print(keys)
+del keys[1:13]
+del keys[27:-1]
+del keys[-1]
+new_keys = []
+
+for key in keys:
+    new_keys.append(key.lower())
+
+new_keys.append('yearly_weekhigh')
+new_keys.append('yearly_weeklow')
+
+print(new_keys)
