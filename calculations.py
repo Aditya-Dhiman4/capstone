a8f64597b9a44a0c324f013055caa121284f97db
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2022-04-22 11:27:40 -0700

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,16 +1,19 @@
 from database import db
 import math
 
+# User inputs stock symbol
+symbol = input(str('Enter Stock: ')).upper()
+
 # getting price and volume data
-time_series_data = db.select_to_df('''
+time_series_data = db.select_to_df(f'''
 select symbol, trade_date, open_price, high_price, low_price, close_price, volume
-from public.all_stock_data where symbol = 'INTC' 
+from public.all_stock_data where symbol = '{symbol}' 
 ''')
 
 # getting other data (ex. P/E Ratio, 52 Week High, etc.)
-overview_data = db.select_to_df('''
+overview_data = db.select_to_df(f'''
 select symbol, marketcapitalization, ebitda, peratio, pegratio, bookvalue, dividendpershare, dividendyield, eps, revenuepersharettm, profitmargin, operatingmarginttm, returnonassetsttm, returnonequityttm, revenuettm, grossprofitttm, dilutedepsttm, quarterlyearningsgrowthyoy, quarterlyrevenuegrowthyoy, analysttargetprice, trailingpe, forwardpe, pricetosalesratiottm, pricetobookratio, evtorevenue, evtoebitda, beta, 52weekhigh, 52weeklow, yearly_weekhigh, yearly_weeklow 
-from public.overview_data where symbol = 'INTC'
+from public.overview_data where symbol = '{symbol}'
 ''')
 
 # Retrieving Beta from dataframe
